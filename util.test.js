81bdeac21458b16b8f4a350ccad6deec9b93927f
sehermahmud <seher.mahmud@gmail.com>
2020-07-28 13:02:03 +0600

    Added

---
+++ util.test.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const { generateText, checkAndGenerate } = require('./util');
+
+test('should output name and age', () => {
+  const text = generateText('Max', 29);
+  expect(text).toBe('Max (29 years old)');
+});
+
+test('should generate a valid text output', () => {
+  const text = checkAndGenerate('Max', 29);
+  expect(text).toBe('Max (29 years old)');
+});
+
+test('should create an element with text and correct class', async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    // slowMo: 80,
+    // args: ['--window-size=1920,1080']
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    'file:///Users/sehermahmud/npm-global/udemy/3.%20javasrcipt-the%20complete%20guide/testing2/index.html'
+  );
+  await page.click('input#name');
+  await page.type('input#name', 'Anna');
+  await page.click('input#age');
+  await page.type('input#age', '28');
+  await page.click('#btnAddUser');
+  const finalText = await page.$eval('.user-item', el => el.textContent);
+  expect(finalText).toBe('Anna (28 years old)');
+}, 10000);
